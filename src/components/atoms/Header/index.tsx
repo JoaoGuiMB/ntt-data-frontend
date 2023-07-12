494e0a3ae4d6031749ca9a6923fd5cbbe5e3e8b1
JoaoGuiMB <juaogui159@gmail.com>
2023-07-12 16:25:21 -0300

    styles: change logo

--- src/components/atoms/Header/index.tsx
+++ src/components/atoms/Header/index.tsx
@@ -1,11 +1,5 @@
 import { ShellBar } from "@ui5/webcomponents-react";
 
 export default function Header() {
-  const APP_NAME = "NTTMovies";
-  return (
-    <ShellBar
-      logo={<img src="/nttMoviesLogo.png" alt={`${APP_NAME}'s logo`} />}
-      primaryTitle={APP_NAME}
-    />
-  );
+  return <ShellBar primaryTitle="NTTmovies" />;
 }
--- src/components/molecules/AppDescription/index.tsx
+++ src/components/molecules/AppDescription/index.tsx
@@ -1,9 +1,13 @@
 import { FlexBox, Title, Text } from "@ui5/webcomponents-react";
+import "./styles.scss";
 
 export default function AppDescription() {
   return (
     <FlexBox direction="Column" justifyContent="Center" alignItems="Center">
-      <Title level="H1">NTTmovies</Title>
+      <div id="logoContainer">
+        <img src="/NTTmoviesWhite.png" alt="Logo" />
+        <Title level="H1">NTTmovies</Title>
+      </div>
       <Text style={{ fontSize: 16 }}>
         NTTmovies is a web application that allows you to search for movies and
         series and see their details.
